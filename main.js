9b08b7b7184f923fa5d028be1524b3f9839d5a62
annaabbott <annaabbottdesign@gmail.com>
2019-03-24 22:56:51 -0600

    import statements added

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import { senate } from './senators.json'
+import { house } from '.representatives.json'
+
 var card = document.querySelector('.card');
 card.addEventListener( 'mouseover', function() {
   card.classList.toggle('is-flipped');
