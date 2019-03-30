75f01190800a0d53bdc5403f9e34ca1032c0cb14
annaabbott <annaabbottdesign@gmail.com>
2019-03-29 23:44:25 -0600

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ var card = document.querySelector('.card');
 card.addEventListener( 'mouseover', function() {
   card.classList.toggle('is-flipped');
 });
+
+/*
+
+TODO:
+  * Figure out where to get pictures of all the senators I want to represent
+  * Figure out what stats I want on the back
+      * Name
+      * Party
+      * home state
+      * date elected
+      * number of terms
+      * website
+      * age
+  * Come up with a function that can generate DOM elements for the picture
+  * Come up with a function that can generate DOM elements for the stats
+*/
