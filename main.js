4e149d35a884e930023df441d892cd18fd470065
annaabbott <annaabbottdesign@gmail.com>
2019-03-30 20:31:14 -0600

    Logic to build cards that flip

--- main.js
+++ main.js
@@ -1,24 +1,57 @@
 import { senate } from './senate.js'
 import { house } from './house.js'
 
-// Set an event listener for mouseover
-var card = document.querySelector('.card');
-card.addEventListener( 'mouseover', function() {
-  card.classList.toggle('is-flipped');
-});
 
-/*
 
-TODO:
-  * Figure out where to get pictures of all the senators I want to represent
-  * Figure out what stats I want on the back
-      * Name
-      * Party
-      * home state
-      * date elected
-      * number of terms
-      * website
-      * age
-  * Come up with a function that can generate DOM elements for the picture
-  * Come up with a function that can generate DOM elements for the stats
-*/
+const cardListDiv = document.getElementById("card-list");
+
+for (let i = 0; i < senate.length; i++) {
+  const item = senate[i];
+  const card = MakeSenatorCard(item);
+  cardListDiv.appendChild(card);
+}
+
+
+
+
+
+function MakeSenatorCard(senator) {
+  // Create the <div> for the scene card
+  const sceneCard = document.createElement('div');
+  sceneCard.classList.add("scene", "scene--card");
+
+  // Create the <div> for the card
+  const card = document.createElement('div');
+  card.classList.add("card");
+  card.addEventListener( 'click', function() {
+    card.classList.toggle('is-flipped');
+  });
+  sceneCard.appendChild(card);
+
+  // Create the <div> for the front of the card
+  const frontFace = MakeCardFront(senator);
+  card.appendChild(frontFace);
+
+  // Create the <div> for the back of the card
+  const backFace = MakeCardBack(senator);
+  card.appendChild(backFace);
+
+  return sceneCard;
+}
+
+function MakeCardFront(senator) {
+  // Create the <div> for the front of the card
+  const frontFace = document.createElement('div');
+  frontFace.classList.add("card__face", "card__face--front");
+  frontFace.innerText = senator.first_name;
+
+  return frontFace;
+}
+
+function MakeCardBack(senator) {
+  const backFace = document.createElement('div');
+  backFace.classList.add("card__face", "card__face--back");
+  backFace.innerText = senator.last_name;
+
+  return backFace;
+}
